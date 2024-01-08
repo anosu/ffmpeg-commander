03a71897e3e1868c37ee59d5155b6055ece9c0c3
Jitsu <60904693+anosu@users.noreply.github.com>
2024-01-08 19:21:37 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,8 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/ffmpeg-commander/'
-    : '/',
+  // publicPath: process.env.NODE_ENV === 'production'
+  //   ? '/ffmpeg-commander/'
+  //   : '/',
+  publicPath: '/',
 });
